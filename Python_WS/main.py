62ef7a95ed0849afcc1f69dcc3a5b0eff50183b9
Daniel Maidment <maidment.daniel@gmail.com>
2020-09-18 14:32:40 +0200

    Deleted some unnecessary code

--- Python_WS/main.py
+++ Python_WS/main.py
@@ -3,16 +3,11 @@ from datetime import date
 import time
 
 
-# print(state)
-# print(cur_state)
-# print(filename)
 if __name__ == "__main__":
     filename = "./BPAP_{}_log.txt".format(date.today().strftime('%y_%m_%d'))
     port = "COM4"
     ser = serial.Serial(port, 9600)
     file = open(filename, 'w')
-    file.write(time.ctime(time.time()))
-    file.write('\n')
     N = 4094
     n = 0
     state = {
@@ -26,13 +21,13 @@ if __name__ == "__main__":
     while(n < N):
         if(ser.in_waiting >= 0):
             serData = ser.readline().decode('ascii').strip()
-            # print(serData)
-            # data = uint_8(serData)
             file.write('{},'.format(serData))
-            n = n+1
+            n += 1
     print("DONE")
+
     file.write('\n')
     file.write(time.ctime(time.time()))
     file.write('\n')
+
     ser.close()  # close serial communications opening port for other use
     file.close()
