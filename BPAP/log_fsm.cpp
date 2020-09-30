dfa34bb908ce8fe52b357ab5ecbd0f93fe49e0ff
Daniel Maidment <maidment.daniel@gmail.com>
2020-09-30 15:34:28 +0200

    wrote some log code in log_fsm.cpp

--- BPAP/log_fsm.cpp
+++ BPAP/log_fsm.cpp
@@ -5,6 +5,9 @@ log_enum CUR_LOG_STATE = LOG_START;
 static volatile int _addr = 0;
 static byte _val = 0;
 
+static char _logstring[2] = {0};
+static uint8_t idx = 0;
+
 bool log_FSM(LiquidCrystal * lcdPtr)
 {
     lcdPtr->setCursor(0,0);
@@ -70,4 +73,31 @@ bool writeEEPROM(int x)
     {
         return true;
     }
+}
+
+void writemainstate(int x)
+{
+    _logstring[0] = char(x);
+}
+void writesubstate(int x)
+{
+    _logstring[1] = char(x);
+}
+bool writetoserial(char x)
+{
+    Serial.print(_logstring[0]);
+    Serial.print(',');
+    Serial.print(_logstring[1]);
+    Serial.print(',');
+    Serial.print(round(getBPM()));
+    Serial.print(',');
+    Serial.print((round((getVOL()*100))));
+    Serial.print(',');
+    Serial.print(round(getIE()));
+    Serial.print(',');
+    Serial.print(round(_pressCur));
+    Serial.print(',');
+    Serial.print(round(_pressThresh));
+    Serial.print(',');
+    Serial.println(getAngle());
 }
